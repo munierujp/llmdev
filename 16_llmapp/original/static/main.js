368b2ddb475c9e8d1310b1ceb0d9e56fea0f0d27
Munieru <20086673+munierujp@users.noreply.github.com>
2025-09-11 14:24:21 +0900

    スタイル: 即時関数でウィンドウの読み込みイベントをラップ

--- 16_llmapp/original/static/main.js
+++ 16_llmapp/original/static/main.js
@@ -1,12 +1,14 @@
-window.addEventListener('load', () => {  
-  const chatBoxElement = document.getElementById('chat-box')
-  chatBoxElement.scrollTop = chatBoxElement.scrollHeight
-  const formElement = document.getElementById('chat-form')
-  const textareaElement = document.getElementById('user-input')
-  textareaElement.addEventListener('keydown', event => {
-    if (event.ctrlKey && event.key === 'Enter') {
-      event.preventDefault()
-      formElement.submit()
-    }
-  });
-})
+(() => {
+  window.addEventListener('load', () => {  
+    const chatBoxElement = document.getElementById('chat-box')
+    chatBoxElement.scrollTop = chatBoxElement.scrollHeight
+    const formElement = document.getElementById('chat-form')
+    const textareaElement = document.getElementById('user-input')
+    textareaElement.addEventListener('keydown', event => {
+      if (event.ctrlKey && event.key === 'Enter') {
+        event.preventDefault()
+        formElement.submit()
+      }
+    });
+  })
+})()
