1459267b8fa41a313c803df7a2ecb885f6844817
Munieru <20086673+munierujp@users.noreply.github.com>
2025-09-11 16:59:13 +0900

    スタイル: ルートパスのPOSTリクエストを送信メッセージ用のエンドポイントに変更し、メッセージ処理を改善

--- 16_llmapp/original/app.py
+++ 16_llmapp/original/app.py
@@ -4,34 +4,42 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import uuid
-from flask import Flask, render_template, request, make_response, session 
+from flask import Flask, render_template, request, make_response, session, redirect, url_for
 from chatbot.graph import get_bot_response, get_messages_list, memory
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+  messages = []
+
   if 'thread_id' not in session:
     session['thread_id'] = str(uuid.uuid4())
-
-  match request.method:
-    case 'GET':
-      memory.storage.clear()
-      response = make_response(render_template('index.html', messages=[]))
-      return response
-    case 'POST':
-      user_message = request.form['user_message']
-      get_bot_response(user_message, memory, session['thread_id'])
+    memory.storage.clear()
+  else:
+    try:
       messages = get_messages_list(memory, session['thread_id'])
-      return make_response(render_template('index.html', messages=messages))
+    except:
+      messages = []
+  
+  response = make_response(render_template('index.html', messages=messages))
+  return response
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+  if 'thread_id' not in session:
+    session['thread_id'] = str(uuid.uuid4())
+  
+  user_message = request.form['user_message']
+  get_bot_response(user_message, memory, session['thread_id'])
+  return redirect(url_for('index'))
 
 @app.route('/clear', methods=['POST'])
 def clear():  
   session.pop('thread_id', None)
   memory.storage.clear()
-  response = make_response(render_template('index.html', messages=[]))
-  return response
+  return redirect(url_for('index'))
 
 if __name__ == '__main__':
   app.run(debug=True)
